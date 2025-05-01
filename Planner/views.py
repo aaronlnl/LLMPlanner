67c5bc625b5d3b9a4808adc530e45c7a956d0865
aaronlnl <aaronlnlwm@gmail.com>
2025-05-01 16:28:33 +0100

    Initialized Django project.

---
+++ Planner/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+import calendar
+import datetime
+
+
+
+# Create your views here.
+def index(request):
+    return render(request, "planner/index.html")
+
+def cal(request):
+    today = datetime.date.today()
+    dates = [ (today + datetime.timedelta(i)) for i in range(-1 - today.weekday(), 6 - today.weekday())]    
+
+    return render(request, "planner/calendar.html", {
+        "dates": dates
+    })
